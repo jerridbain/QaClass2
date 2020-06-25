1112d59637878ec4397ac896819661094f18b997
Jerrid Bain <jerridbain@gmail.com>
2020-06-25 19:48:44 -0400

    files

--- test/specs/a.smoke/smoke.dropdown.spec.js
+++ test/specs/a.smoke/smoke.dropdown.spec.js
@@ -1,27 +1,35 @@
-const drp = require('../../pages/dropdown.page')
+const dropDown = require('../../pages/dropDown.page')
 
-describe('Smoke test Dropdown page', () => {
-    
-    it('Given I go to the Dropdown page', () => {
-        drp.goToDropDown();
+describe('As a user, test the dropdown page\'s default setting', () => {
+
+    it('Given I go to the dropdown page', () => {
+        dropDown.goToDropDown()
 
     });
 
-    it('Then validate the Dropdown header exist', () => {
-        drp.header().isExisting();
+   it('Then validate that no option is selected', () => {
+       dropDown.select().$$('option')[0].isSelected().must.be.true()
+       dropDown.select().$$('option')[0].getText().must.equal('Please select an option')
+
+
+
+   });
+   it('And validate that all the other options are not selected', () => {
+       dropDown.select().$$('option').forEach((opt) => {
+
+        if (opt.getText() !== 'Please select an option') {
+            opt.isSelected().must.be.false()
+        }
+
+       })
+
+   });
+
+   
         
-    });
-
-    it('And validate the paragraphs exist', () => { 
-        drp.id()[0].isExisting()
-        
-
-    });
-    
+})
     
 
     
 
 
-
-});
